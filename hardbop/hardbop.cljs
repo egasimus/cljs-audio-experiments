e0a7252e23484c7cff9badbd69172ebcdb7a8458
Adam Avramov <fallenblood@gmail.com>
2014-11-20 01:42:47 +0200

    Successful liftoff into ClojureScript.

--- hardbop/hardbop.cljs
+++ hardbop/hardbop.cljs
@@ -1,23 +1,35 @@
 (ns hardbop.core
   (:require [cljs.repl :as repl]))
 
+(def *js-path*      (js/require "path"))
+
+(def *bop-cwd*      (.cwd js/process))
+
+(def *bop-session*  (if-let [session (nth (.-argv js/process) 2)]
+                      (.resolve *js-path* session)
+                      "untitled"))
+
 (defn pep [text]
   (println (str (repl/prompt) text))
   (repl/eval-print text)
   (println))
 
 (defn -main [& args]
+
+  ;; we're home
   (repl/init)
 
-  ;; setup the print function
+  ;; setup print functions
   (set! *out* #(.write (.-stdout js/process) %))
   (set! *rtn* #(.write (.-stdout js/process) %))
   (set! *err* #(.write (.-stderr js/process) %))
   (set! *print-fn* #(*out* %))
 
-  ;; greet the user
+  ;; where are our manners?
   (println "\nbopbop boq doqdoq")
+  (println *bop-session*)
 
+  ;; setup readline interface to repl
   (let [readline (js/require "readline")
         rl (.createInterface readline js/process.stdin js/process.stdout)]
     (.setPrompt rl (repl/prompt))
@@ -32,7 +44,5 @@
     (.on rl "close" (fn []
       (println "\nqodqoq pod bopbop")
       (.exit js/process 0)))))
-
-  ;; this is gonna be printed on successful exit
   
 (set! *main-cli-fn* -main)
