20a734006167122f37359cc493739158db598bbb
Adam Avramov <fallenblood@gmail.com>
2014-11-20 03:23:14 +0200

    *refactor-refactor*

--- hardbop/repl.cljs
+++ hardbop/repl.cljs
@@ -1,16 +1,40 @@
 (ns hardbop.repl
   (:require [cljs.repl :as repl]))
 
+
+(def HELLO   "!bopbop boq doqdoq¡")
+
+(def GOODBYE "?qodqoq pod bopbop¿")
+
+
 (def *bop-cwd*      (.cwd js/process))
 
 (def *bop-session*  (if-let [session (nth (.-argv js/process) 2)]
                       (.resolve (js/require "path") session)
                       "untitled"))
 
-(defn pep [text]
-  (println (str (repl/prompt) text))
-  (repl/eval-print text)
-  (println))
+(defn evaluate [path]
+  (let [fs (js/require "fs")
+        text (.toString (.readFileSync fs *bop-session*))]
+    (repl/eval-print text)))
+
+
+(defn run-repl []
+  (let [readline (js/require "readline")
+        rl (.createInterface readline js/process.stdin js/process.stdout)]
+    (.setPrompt rl (repl/prompt))
+    (.prompt rl)
+    (.on rl "line" (fn [line]
+                     (when (seq (filter #(not= " " %) line))
+                       (repl/eval-print line)
+                       (println))
+                     (.setPrompt rl (repl/prompt))
+                     (.prompt rl)))
+
+    (.on rl "close" (fn []
+      (println (str "\n\n" GOODBYE))
+      (.exit js/process 0)))))
+
 
 (defn -main [& args]
 
@@ -24,30 +48,15 @@
   (set! *print-fn* #(*out* %))
  
   ;; evaluate the session contents 
-  (let [fs (js/require "fs")
-        text (.toString (.readFileSync fs *bop-session*))]
-    (repl/eval-print text))
+  (evaluate *bop-session*)
 
   ;; oh my, where are our manners?
-  (println "\n\nbopbop boq doqdoq!\n")
+  (println (str "\n\n" HELLO "\n"))
   (println "*bop-cwd*     " *bop-cwd*)
   (println "*bop-session* " *bop-session*)
   (println)
 
   ;; setup readline interface to repl
-  (let [readline (js/require "readline")
-        rl (.createInterface readline js/process.stdin js/process.stdout)]
-    (.setPrompt rl (repl/prompt))
-    (.prompt rl)
-    (.on rl "line" (fn [line]
-                     (when (seq (filter #(not= " " %) line))
-                       (repl/eval-print line)
-                       (println))
-                     (.setPrompt rl (repl/prompt))
-                     (.prompt rl)))
-
-    (.on rl "close" (fn []
-      (println "\n\nqodqoq pod bopbop?")
-      (.exit js/process 0)))))
+  (run-repl))
   
 (set! *main-cli-fn* -main)
